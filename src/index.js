65fdb9088b77831546a1f9c9d0d7935acb7e1629
Hermloth <herm_loth@hotmail.com>
2024-10-31 01:06:01 +1000

    add initial factory function and prototype

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './style.css';
 
+const LinkedListProto = {
+    testfunction (){
+        console.log(`Test Function Called on ${this.name}!`)
+    }
+
+// implement proto methods
+
+}
+
+function CreateListItem (name){
+    return Object.create(LinkedListProto, {
+        name: {
+            value: name
+        }
+    })
+}
+
+const testVar = CreateListItem(`TestObject`);
+
+testVar.testfunction()
+
+
 
