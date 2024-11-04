08b7e4d4d2cb73dc222d5a486735aba13291df26
Hermloth <herm_loth@hotmail.com>
2024-11-04 23:14:31 +1000

    added first functions

--- src/index.js
+++ src/index.js
@@ -1,25 +1,91 @@
 import './style.css';
 
-const LinkedListProto = {
-    testfunction (){
-        console.log(`Test Function Called on ${this.name}!`)
-    }
 
-// implement proto methods
-
-}
-
-function CreateListItem (name){
+function LinkedList (){
     return Object.create(LinkedListProto, {
-        name: {
-            value: name
+        value: {
+            head: null,
+            size: 0
         }
     })
 }
 
-const testVar = CreateListItem(`TestObject`);
+const LinkedListProto = {
+    testfunction (){
+        console.log(`Test Function Called on ${this.value}!`)
+    },
 
-testVar.testfunction()
+// Add First Element (Not working right)
+    AddFirstElement(value, next = null){
+        this.head = Object.create({
+            value: {
+                value: value,
+                next: next
+            }
+        })
+        this.size++;
+    },
+
+// Add Last Element
+    AddLastElement(value){
+        let current;
+        if (!this.head){
+            this.head = Object.create({
+                value: {
+                    value: value,
+                    next: null
+                }
+            })
+        } else {
+            let node = Object.create({
+                value: {
+                    value: value,
+                    next: null
+                }
+            })
+            //////// Currently adds next property to head, needs to add to last created object
+            this.head.next = node;
+            this.size++;
+        }
+    },
+// Return Size
+    ReturnSize(){
+        console.log(this.size);
+    },
+// Return Head Node
+
+// Return Tail Node
+
+// Return Node at Index
+
+// Remove Last Element
+
+// Check if value exists (True / False)
+
+//find index of value that exists
+
+// Return list Object as a String
+    returnlist (){
+        let current = this.head;
+/*        while (current) {
+            console.log(current.value);
+            console.log(current.next);
+        }        
+  */      
+        console.log(this);
+    }
+}
+
+const ll = LinkedList();
+
+ll.AddFirstElement(`TestObject1`);
+ll.AddFirstElement(`TestObject2`);
+//ll.AddLastElement(`TestObject3`);
+//console.log(ll);
+ll.returnlist();
+//const testVar = LinkedList(`TestObject`);
+//testVar.returnlist();
+//console.log(testVar)
 
 
 
